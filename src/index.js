164a1854f364f728a63510cbf70b852c1133c212
Yevhen_Palamarchuk <frankeshtayn00@gmail.com>
2023-08-17 19:39:20 +0300

    fix: just commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const stack = [];
+  const openingBrackets = new Set(bracketsConfig.map(pair => pair[0]));
+  const closingBrackets = new Set(bracketsConfig.map(pair => pair[1]));
+  const bracketPairs = Object.fromEntries(bracketsConfig);
+
+  for (let bracket of str) {
+    if (openingBrackets.has(bracket)) {
+      // Handle opening brackets
+      if (bracketPairs[bracket] !== bracket) {
+        // Handle cases where opening and closing brackets are the same
+        stack.push(bracket);
+      } else {
+        if (stack.length === 0 || stack[stack.length - 1] !== bracket) {
+          stack.push(bracket);
+        } else {
+          stack.pop();
+        }
+      }
+    } else if (closingBrackets.has(bracket)) {
+      // Handle closing brackets
+      if (stack.length === 0 || bracketPairs[stack[stack.length - 1]] !== bracket) {
+        return false;
+      }
+      stack.pop();
+    }
+  }
+
+  return stack.length === 0;
+};
